72f02564c8ba42cfe7be58b92211ea29e7ea4926
Luyanda-22 <luyandamascot@gmail.com>
2023-06-14 06:47:13 +0200

    Made string to num + added conditional statements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What gade percentage did you get on your last exam?");
+        string number = Console.ReadLine();
+        int percentage = int.Parse(number);
+
+        string letter; 
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"The letter grade is: {letter}.");
+
+        if (percentage >=70)
+        {
+            Console.WriteLine("Congradulations! You passed the exam.");
+        }
+
+        else
+        {
+            Console.WriteLine("Study hard next time champ.");
+        }
+
+
     }
 }
