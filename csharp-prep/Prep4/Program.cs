b733e21fb0474b6e65ce2712659bfc0fc291c029
Luyanda-22 <luyandamascot@gmail.com>
2023-06-15 09:42:25 +0200

    Created and manipulated a list of numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+       
+        List<int> numberList = new List<int>();
+
+        int userNumber;
+
+        while(true)
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber == 0)
+            {
+                break;
+            }
+
+            numberList.Add(userNumber);
+        }
+
+        int sum = 0;
+        foreach (int number in numberList)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+
+        double average = (double)sum / numberList.Count;
+
+        Console.WriteLine($"The average number is: {average}");
+
+        int largestNumber = numberList.Max();
+        Console.WriteLine($"The Largest number in the list is: {largestNumber}");
+
+
     }
 }
