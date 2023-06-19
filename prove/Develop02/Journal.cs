da9150e1e40d5bdacebec62631b6816ce8dc98d1
Luyanda-22 <luyandamascot@gmail.com>
2023-06-19 21:16:20 +0200

    Moved files to DEV 2

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,29 @@
+public class Journal
+{
+    public List<Entry> _entries;
+
+    public void DisplayEntries()
+    {
+
+    }
+
+    public void EntriesIn (Entry newEntry)
+    {
+
+    }
+
+    public void LoadFIle(string file)
+    {
+
+    }
+
+    public void SaveFile(string file)
+    {
+
+    }
+
+    public void DeleteEntry()
+    {
+        
+    }
+}
