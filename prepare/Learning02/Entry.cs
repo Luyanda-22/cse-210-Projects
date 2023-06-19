da0831376cda107a020d307e7c3ebe8a804ea6b1
Luyanda-22 <luyandamascot@gmail.com>
2023-06-19 20:52:40 +0200

    Created three classes and stub codes

---
+++ prepare/Learning02/Entry.cs
@@ -0,0 +1,11 @@
+public class Entry
+{
+    public string _date;
+    public string _question;
+    public string _userEntry;
+
+    public void Display()
+    {
+        
+    }
+}
---
+++ prepare/Learning02/Prompt.cs
@@ -0,0 +1,16 @@
+public class Prompt
+{
+    public string _prompt;
+
+    public List<string> _questions;
+
+    public string GetPrompt()
+    {
+        return "";
+    }
+
+    public void Display()
+    {
+        
+    }
+}
